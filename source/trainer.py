f56ecc391dd6513267e4cf55933e73f9f8140613
El Amine <45946838+qdevpsi3@users.noreply.github.com>
2024-08-24 11:16:59 +0800

    Add generator and discriminator losses

--- source/trainer.py
+++ source/trainer.py
@@ -53,10 +53,15 @@ class MolGAN(LightningModule):
         optimizer,
         *,
         grad_penalty=10.0,
-        method="soft_gumbel",
+        process_method="soft_gumbel",
+        m_dim=5,
+        b_dim=5,
     ):
         super().__init__()
-        self.save_hyperparameters(logger=False)
+        self.save_hyperparameters(
+            logger=False,
+            ignore=["generator", "discriminator", "predictor", "optimizer"],
+        )
         self.generator = generator
         self.discriminator = discriminator
         self.predictor = predictor
@@ -83,3 +88,57 @@ class MolGAN(LightningModule):
         gradient_norm = gradients.norm(2, dim=1)
         grad_penalty = ((gradient_norm - 1) ** 2).mean()
         return grad_penalty * self.hparams.grad_penalty
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        x, _ = batch
+        # train generator
+        if optimizer_idx == 0:
+            return self.generator_step(x)
+        # train discriminator
+        if optimizer_idx == 1:
+            return self.discriminator_step(x)
+        return None
+
+    def _generate_noise(self, batch_size):
+        return torch.rand(batch_size, self.hparams.z_dim, device=self.device)
+
+    def _generate_fake_data(self, batch):
+        z = self._generate_noise(batch.size(0))
+        a_fake, x_fake = self.generator(z)
+        return a_fake, x_fake
+
+    def _process_real_data(self, a_real, x_real):
+        a_real = label2onehot(a_real, self.hparams.b_dim)
+        x_real = label2onehot(x_real, self.hparams.m_dim)
+        return a_real, x_real
+
+    def _process_fake_data(self, a_fake, x_fake):
+        a_fake, x_fake = postprocess(
+            [a_fake, x_fake],
+            method=self.hparams.process_method,
+        )
+        return a_fake, x_fake
+
+    def _apply_discriminator(self, a, x):
+        return self.discriminator(a, None, x)[0]
+
+    def _compute_discriminator_loss(self, batch):
+        a_real, x_real = batch["A"], batch["X"]
+        a_real, x_real = self._process_real_data(a_real, x_real)
+        a_fake, x_fake = self._generate_fake_data(batch)
+        a_fake, x_fake = a_fake.detach(), x_fake.detach()  # detach fake data
+        a_fake, x_fake = self._process_fake_data(a_fake, x_fake)
+        d_fake = self._apply_discriminator(a_fake, x_fake)
+        d_real = self._apply_discriminator(*batch)
+        grad_penalty = self._calculate_gradient_penalty(
+            torch.cat([a_real, a_fake]), torch.cat([x_real, x_fake])
+        )
+        d_loss = d_fake.mean() - d_real.mean() + grad_penalty
+        return d_loss
+
+    def _compute_generator_loss(self, batch):
+        a_fake, x_fake = self._generate_fake_data(batch)
+        a_fake, x_fake = self._process_fake_data(a_fake, x_fake)
+        d_fake = self._apply_discriminator(a_fake, x_fake)
+        g_loss = -d_fake.mean()
+        return g_loss
