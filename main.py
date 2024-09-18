980c03da3f8fa2ee3174fc0d4b5227c3a92b0184
El Amine <45946838+qdevpsi3@users.noreply.github.com>
2024-09-18 19:59:48 +0200

    Refactor dataset loading and setup process

--- main.py
+++ main.py
@@ -3,56 +3,24 @@ from functools import partial
 from lightning import Trainer
 from torch.optim import Adam
 
-from source.datasets import SparseMolecularDataModule, SparseMolecularDataset
+from source.datasets import MolecularDataModule, MolecularDataset
 from source.nets import Discriminator, Generator
 from source.trainer import MolGAN
 
 if __name__ == "__main__":
     # Create data module
-    datamodule = SparseMolecularDataModule(
-        "./data/gdb9.sdf",
-        max_atoms=9,
-        add_h=False,
-    )
+    dataset = MolecularDataset.load("./data/gdb9_molecular_dataset.pkl")
+    datamodule = MolecularDataModule(dataset, batch_size=32)
     datamodule.setup()
 
-    complexity = "nr"
-    if complexity == "nr":
-        g_conv_dim = [128, 256, 512]
-    elif complexity == "mr":
-        g_conv_dim = [128]
-    elif complexity == "hr":
-        g_conv_dim = [16]
-    else:
-        raise ValueError(
-            "Please enter an valid model complexity from 'mr', 'hr' or 'nr'!"
-        )
-    z_dim = 8
-    num_vertices = datamodule.dataset.num_vertices
-    bond_num_types = datamodule.dataset.bond_num_types
-    atom_num_types = datamodule.dataset.atom_num_types
-    print(
-        f"num_vertices: {num_vertices}, bond_num_types: {bond_num_types}, atom_num_types: {atom_num_types}"
-    )
-    dropout = 0.0
-
-    d_conv_dim = [[128, 64], 128, [128, 64]]
-    m_dim = atom_num_types
-    b_dim = bond_num_types
     # Create generator
-    G = Generator(
-        g_conv_dim,
-        z_dim,
-        num_vertices,
-        bond_num_types,
-        atom_num_types,
-        dropout,
-    )
-    D = Discriminator(d_conv_dim, m_dim, b_dim - 1, dropout)
-    V = Discriminator(d_conv_dim, m_dim, b_dim - 1, dropout)
+    G = Generator(dataset)
+    D = Discriminator(dataset)
+    V = Discriminator(dataset)
     print("Model created")
 
     model = MolGAN(
+        dataset,
         G,
         D,
         V,
